e9ee4558cfb0cc82676844cca4e13c30d4b3f5dc
huypham6688 <bilabu44@gmail.com>
2025-09-14 22:41:46 +0700

    update even

--- app/Filament/Resources/EventResource/Pages/ListEvents.php
+++ app/Filament/Resources/EventResource/Pages/ListEvents.php
@@ -3,11 +3,18 @@
 namespace App\Filament\Resources\EventResource\Pages;
 
 use App\Filament\Resources\EventResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListEvents extends ListRecords
 {
     protected static string $resource = EventResource::class;
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
 
 
